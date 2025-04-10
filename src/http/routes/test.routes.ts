2ff2a824cc30b2ec2cc2d8de2dcc00b13cb746b4
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-04-10 19:48:41 -0300

    feat: adição de rota de teste para simulação de erros no Sentry com configuração de esquema para ambiente de desenvolvimento

--- src/http/routes/test.routes.ts
+++ src/http/routes/test.routes.ts
@@ -2,13 +2,16 @@ import { FastifyInstance } from 'fastify'
 import { env } from '@/env'
 
 export async function testRoutes(app: FastifyInstance) {
-  console.log(env.NODE_ENV)
-
-  if (env.NODE_ENV === 'dev') {
-    app.get('/test-error', async () => {
+  app.get('/test-error', {
+    schema: {
+      hide: env.NODE_ENV !== 'dev',
+      tags: ['Test'],
+      summary: 'Test error route for Sentry',
+    },
+    handler: async () => {
       // Simulando um erro para o sentry
       // http://localhost:3333/test-error
       throw new Error('Este é um erro de teste para o Sentry')
-    })
-  }
+    },
+  })
 }
