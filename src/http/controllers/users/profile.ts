edc8553dd52d8bc5702926236541b43603b9f39e
GabrielVitor08 <gabrieldatas2004@gmail.com>
2025-06-14 15:23:26 -0300

    refactor: renomear parâmetro de userId para id e aprimorar a lógica de busca de perfil de usuário com verificação de autorização

--- src/http/controllers/users/profile.ts
+++ src/http/controllers/users/profile.ts
@@ -5,7 +5,7 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
   const getUserProfile = makeGetUserProfileUseCase()
 
   const { user } = await getUserProfile.execute({
-    userId: request.user.sub,
+    id: request.user.sub,
   })
 
   return reply.status(200).send({
--- src/use-cases/getUserProfile/get-user-profile.ts
+++ src/use-cases/getUserProfile/get-user-profile.ts
@@ -1,29 +1,73 @@
 import { UsersRepository } from '@/repositories/users-repository'
-import { User } from '@prisma/client'
+import {
+  User,
+  type Administrator,
+  type Client,
+  type Professional,
+} from '@prisma/client'
 import { ResourceNotFoundError } from '../@errors/resource-not-found'
+import { NotAuthorizedError } from '../@errors/not-authorized-error'
 
 interface GetUserProfileUseCaseRequest {
-  userId: string
+  id: string
 }
 
 interface GetUserProfileUseCaseResponse {
-  user: User
+  user: {
+    User: User
+    profileData: Administrator | Professional | Client
+  }
 }
-
 export class GetUserProfileUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
   async execute({
-    userId,
+    id,
   }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
-    const user = await this.usersRepository.findById(userId)
+    console.log('GetUserProfileUseCase executed with id:', id)
+    const user = await this.usersRepository.findById(id)
 
     if (!user) {
       throw new ResourceNotFoundError()
     }
 
-    return {
-      user,
+    const authenticatedUser = await this.usersRepository.findById(id)
+
+    if (!authenticatedUser) {
+      throw new NotAuthorizedError()
     }
+
+    if (authenticatedUser.role === 'ADMIN') {
+      const profileData = await this.usersRepository.findAdmByUserId(
+        authenticatedUser.id,
+      )
+
+      if (!profileData) {
+        throw new ResourceNotFoundError()
+      }
+
+      return { user: { User: user, profileData } }
+    }
+
+    if (authenticatedUser.role === 'PROFESSIONAL') {
+      if (user.role !== 'CLIENT') {
+        throw new NotAuthorizedError()
+      }
+      const profileData = await this.usersRepository.findClientByUserId(user.id)
+      if (!profileData) {
+        throw new ResourceNotFoundError()
+      }
+      return { user: { User: user, profileData } }
+    }
+
+    if (authenticatedUser.role === 'CLIENT') {
+      const profileData = await this.usersRepository.findClientByUserId(user.id)
+      if (!profileData) {
+        throw new ResourceNotFoundError()
+      }
+      return { user: { User: user, profileData } }
+    }
+
+    throw new NotAuthorizedError()
   }
 }
